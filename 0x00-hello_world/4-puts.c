f75d8cc65f8049766c583c0bc8159efc89947d42
NajimBusari <albusarinajim01@gmail.com>
2024-08-27 23:34:10 +0000

    printing string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to std output
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
