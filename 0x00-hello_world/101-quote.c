dff7bece49fb098423b239f9481fcd604bfcaa75
NajimBusari <albusarinajim01@gmail.com>
2024-08-30 14:15:14 +0000

    prints quote to stderr without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,17 @@
 #include <unistd.h>
 
+/**
+ * main - prints quote to stderr without printf or puts
+ *
+ * Return: 1
+ */
+
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+	const char *message = 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, message, 59);
-	
+
 	return (1);
 }
