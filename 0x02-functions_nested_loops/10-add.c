0a8a87e55833d225592cbf1764dbfa398727cf8d
NajimBusari <albusarinajim01@gmail.com>
2024-09-06 12:54:14 +0000

    adds 2 integers and return the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -13,16 +13,5 @@ int add(int a, int b)
 	int sum;
 
 	sum = a + b;
-	if (sum < 0)
-	{
-		_putchar('-');
-		sum = -sum;
-	}
-	if (sum >= 10)
-	{
-		_putchar((sum / 10) + '0');
-	}
-	_putchar((sum % 10) + '0');
-
 	return (sum);
 }
