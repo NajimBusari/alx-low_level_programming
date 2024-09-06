6fc86f9f56740981fa2f0ff27830fc6013d57290
NajimBusari <albusarinajim01@gmail.com>
2024-09-06 14:53:26 +0000

    prints n times table, starting from 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,12 +9,7 @@
 void print_times_table(int n)
 {
 	int i, j, mul;
-
-	if ((n < 0) || (n > 15))
-	{
-		return;
-	}
-	else
+	if ((n >= 0) && (n <= 15))
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -22,9 +17,7 @@ void print_times_table(int n)
 			{
 				mul = i * j;
 				if (j == 0)
-				{
 					_putchar(mul + '0');
-				}
 				else if (mul < 10)
 				{
 					_putchar(',');
