488bcf701b43995a32f45af48fa833609d2db21d
NajimBusari <albusarinajim01@gmail.com>
2024-09-06 09:35:43 +0000

    checks if character is a letter, uppercase, or lowercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,14 @@
 /**
  * _isalpha - checks if character is a letter, lowercase or uppercase
  * @c: character to check
- *                                                                                                                                               * Return: 1 if c is a letter, lowercase or uppercase; 0 if otherwise
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase; 0 if otherwise
  */
 
 int _isalpha(int c)
 {
-        if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') ||
-		       	(c >= 'A' && c <= 'Z'))
+	if ((c >= '0' && c < '9') || (c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
